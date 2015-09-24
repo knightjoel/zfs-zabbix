9c80306218c0c9b0a67d82f2610ae05e91c61443
Matúš Bursa <burso@burso.eu>
2015-09-24 11:02:08 +0200

    send error message to stderr; zfs_zabbix.sh ignores stderr now

--- scripts/zfs_zabbix.sh
+++ scripts/zfs_zabbix.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # zabbix zpool's monitoring 
 
 zio=$(which zio)
 
 if [[ -n "$1" ]]; then
                 if [[ "$1" == "devices" ]]; then
-                        $zio -s devices -f json
+                        $zio -s devices -f json 2>/dev/null
                         exit 0
                 fi
 
                 if [[ "$1" == "device-state" ]]; then
-                        $zio -s device-state -d $2
+                        $zio -s device-state -d $2 2>/dev/null
                         exit 0
                 fi
 
@@ -26,37 +26,37 @@ if [[ -n "$1" ]]; then
                 fi
                 zpool="$1"      # set zpool
 else
-        $zio -s pools -f json
+        $zio -s pools -f json 2>/dev/null
         exit 0
 fi
 
 
 dedup() {
-        used=$($zio -s used -z $zpool)
-        dedupratio=$($zio -s dedupratio -z $zpool)
+        used=$($zio -s used -z $zpool 2>/dev/null)
+        dedupratio=$($zio -s dedupratio -z $zpool 2>/dev/null)
         dedup=$(echo $used / $dedupratio | bc)
         echo $used - $dedup | bc
 }
 
 total() {
         dedup=$(dedup)
-        compress=$($zio -s compress -z $zpool)
+        compress=$($zio -s compress -z $zpool 2>/dev/null)
         echo $compress + $dedup | bc
 }
 
 
 if [[ -n "$2" ]]; then
-        if      [[ "$2" == "ioread"     ]]; then $zio -s read_ops -z $zpool
-        elif    [[ "$2" == "iowrite"    ]]; then $zio -s write_ops -z $zpool
-        elif    [[ "$2" == "bsread"     ]]; then $zio -s read_bts -z $zpool
-        elif    [[ "$2" == "bswrite"    ]]; then $zio -s write_bts -z $zpool
-        elif    [[ "$2" == "free"       ]]; then $zio -s available -z $zpool
-        elif    [[ "$2" == "real"       ]]; then $zio -s real -z $zpool
-        elif    [[ "$2" == "compress"   ]]; then $zio -s compress -z $zpool
-        elif    [[ "$2" == "logical"    ]]; then $zio -s logical -z $zpool
+        if      [[ "$2" == "ioread"     ]]; then $zio -s read_ops -z $zpool 2>/dev/null
+        elif    [[ "$2" == "iowrite"    ]]; then $zio -s write_ops -z $zpool 2>/dev/null
+        elif    [[ "$2" == "bsread"     ]]; then $zio -s read_bts -z $zpool 2>/dev/null
+        elif    [[ "$2" == "bswrite"    ]]; then $zio -s write_bts -z $zpool 2>/dev/null
+        elif    [[ "$2" == "free"       ]]; then $zio -s available -z $zpool 2>/dev/null
+        elif    [[ "$2" == "real"       ]]; then $zio -s real -z $zpool 2>/dev/null
+        elif    [[ "$2" == "compress"   ]]; then $zio -s compress -z $zpool 2>/dev/null
+        elif    [[ "$2" == "logical"    ]]; then $zio -s logical -z $zpool 2>/dev/null
         elif    [[ "$2" == "total"      ]]; then total
         elif    [[ "$2" == "dedup"      ]]; then dedup
-        elif    [[ "$2" == "health"     ]]; then $zio -s health -z $zpool
-        elif    [[ "$2" == "ddt"        ]]; then $zio -s ddt-memory -z $zpool
+        elif    [[ "$2" == "health"     ]]; then $zio -s health -z $zpool 2>/dev/null
+        elif    [[ "$2" == "ddt"        ]]; then $zio -s ddt-memory -z $zpool 2>/dev/null
         fi
 fi
--- src/vdev_status.c
+++ src/vdev_status.c
@@ -37,28 +37,28 @@ zpool_print_vdev(zpool_handle_t * zhp, void * data) {
 
     switch(reason) {
         case ZPOOL_STATUS_MISSING_DEV_R:
-            (void) printf("status: One or more devices could not "
+            (void) fprintf(stderr, "status: One or more devices could not "
                                   "be opened.  Sufficient replicas exist for\n\tthe pool to "
                                   "continue functioning in a degraded state.\n");
             break;
 
         case ZPOOL_STATUS_MISSING_DEV_NR:
-            (void) printf("status: One or more devices could not "
+            (void) fprintf(stderr, "status: One or more devices could not "
                                   "be opened.  There are insufficient\n\treplicas for the "
                                   "pool to continue functioning.\n");
             break;
 
         case ZPOOL_STATUS_CORRUPT_LABEL_R:
-            (void) printf("status: One or more devices could not "
+            (void) fprintf(stderr, "status: One or more devices could not "
                                   "be used because the label is missing or\n\tinvalid.  "
                                   "Sufficient replicas exist for the pool to continue\n\t"
                                   "functioning in a degraded state.\n");
-            (void) printf("action: Replace the device using "
+            (void) fprintf(stderr, "action: Replace the device using "
                                   "'zpool replace'.\n");
             break;
 
         case ZPOOL_STATUS_CORRUPT_LABEL_NR:
-            (void) printf("status: One or more devices could not "
+            (void) fprintf(stderr, "status: One or more devices could not "
                                   "be used because the label is missing \n\tor invalid.  "
                                   "There are insufficient replicas for the pool to "
                                   "continue\n\tfunctioning.\n");
@@ -66,108 +66,108 @@ zpool_print_vdev(zpool_handle_t * zhp, void * data) {
             break;
 
         case ZPOOL_STATUS_FAILING_DEV:
-            (void) printf("status: One or more devices has "
+            (void) fprintf(stderr, "status: One or more devices has "
                                   "experienced an unrecoverable error.  An\n\tattempt was "
                                   "made to correct the error.  Applications are "
                                   "unaffected.\n");
-            (void) printf("action: Determine if the device needs "
+            (void) fprintf(stderr, "action: Determine if the device needs "
                                   "to be replaced, and clear the errors\n\tusing "
                                   "'zpool clear' or replace the device with 'zpool "
                                   "replace'.\n");
             break;
 
         case ZPOOL_STATUS_OFFLINE_DEV:
-            (void) printf("status: One or more devices has "
+            (void) fprintf(stderr, "status: One or more devices has "
                                   "been taken offline by the administrator.\n\tSufficient "
                                   "replicas exist for the pool to continue functioning in "
                                   "a\n\tdegraded state.\n");
-            (void) printf("action: Online the device using "
+            (void) fprintf(stderr, "action: Online the device using "
                                   "'zpool online' or replace the device with\n\t'zpool "
                                   "replace'.\n");
             break;
 
         case ZPOOL_STATUS_REMOVED_DEV:
-            (void) printf("status: One or more devices has "
+            (void) fprintf(stderr, "status: One or more devices has "
                                   "been removed by the administrator.\n\tSufficient "
                                   "replicas exist for the pool to continue functioning in "
                                   "a\n\tdegraded state.\n");
 
-            (void) printf("action: Online the device using "
+            (void) fprintf(stderr, "action: Online the device using "
                                   "'zpool online' or replace the device with\n\t'zpool "
                                   "replace'.\n");
             break;
 
         case ZPOOL_STATUS_RESILVERING:
-            (void) printf("status: One or more devices is "
+            (void) fprintf(stderr, "status: One or more devices is "
                                   "currently being resilvered.  The pool will\n\tcontinue "
                                   "to function, possibly in a degraded state.\n");
-            (void) printf("action: Wait for the resilver to complete.\n");
+            (void) fprintf(stderr, "action: Wait for the resilver to complete.\n");
             break;
 
         case ZPOOL_STATUS_CORRUPT_DATA:
-            (void) printf("status: One or more devices has "
+            (void) fprintf(stderr, "status: One or more devices has "
                                   "experienced an error resulting in data\n\tcorruption.  "
                                   "Applications may be affected.\n");
-            (void) printf("action: Restore the file in question "
+            (void) fprintf(stderr, "action: Restore the file in question "
                                   "if possible.  Otherwise restore the\n\tentire pool from "
                                   "backup.\n");
             break;
 
         case ZPOOL_STATUS_CORRUPT_POOL:
-            (void) printf("status: The pool metadata is corrupted "
+            (void) fprintf(stderr, "status: The pool metadata is corrupted "
                                   "and the pool cannot be opened.\n");
             zpool_explain_recover(zpool_get_handle(zhp), zpool_get_name(zhp), reason, config);
             break;
 
         case ZPOOL_STATUS_VERSION_OLDER:
-            (void) printf("status: The pool is formatted using a "
+            (void) fprintf(stderr, "status: The pool is formatted using a "
                                   "legacy on-disk format.  The pool can\n\tstill be used, "
                                   "but some features are unavailable.\n");
 
-            (void) printf("action: Upgrade the pool using 'zpool "
+            (void) fprintf(stderr, "action: Upgrade the pool using 'zpool "
                                   "upgrade'.  Once this is done, the\n\tpool will no longer "
                                   "be accessible on software that does not support\n\t"
                                   "feature flags.\n");
             break;
 
         case ZPOOL_STATUS_VERSION_NEWER:
-            (void) printf("status: The pool has been upgraded to a "
+            (void) fprintf(stderr, "status: The pool has been upgraded to a "
                                   "newer, incompatible on-disk version.\n\tThe pool cannot "
                                   "be accessed on this system.\n");
-            (void) printf("action: Access the pool from a system "
+            (void) fprintf(stderr, "action: Access the pool from a system "
                                   "running more recent software, or\n\trestore the pool from "
                                   "backup.\n");
             break;
 
         case ZPOOL_STATUS_FEAT_DISABLED:
-            (void) printf("status: Some supported features are not "
+            (void) fprintf(stderr, "status: Some supported features are not "
                                   "enabled on the pool. The pool can\n\tstill be used, but "
                                   "some features are unavailable.\n");
-            (void) printf("action: Enable all features using "
+            (void) fprintf(stderr, "action: Enable all features using "
                                   "'zpool upgrade'. Once this is done,\n\tthe pool may no "
                                   "longer be accessible by software that does not support\n\t"
                                   "the features. See zpool-features(5) for details.\n");
             break;
 
         case ZPOOL_STATUS_UNSUP_FEAT_READ:
-            (void) printf("status: The pool cannot be accessed on "
+            (void) fprintf(stderr, "status: The pool cannot be accessed on "
                                   "this system because it uses the\n\tfollowing feature(s) "
                                   "not supported on this system:\n");
             zpool_print_unsup_feat(config);
-            (void) printf("\n");
-            (void) printf("action: Access the pool from a system "
+            (void) fprintf(stderr, "\n");
+            (void) fprintf(stderr, "action: Access the pool from a system "
                                   "that supports the required feature(s),\n\tor restore the "
                                   "pool from backup.\n");
             break;
 
         case ZPOOL_STATUS_UNSUP_FEAT_WRITE:
-            (void) printf("status: The pool can only be accessed "
+            (void) fprintf(stderr, "status: The pool can only be accessed "
                                   "in read-only mode on this system. It\n\tcannot be "
                                   "accessed in read-write mode because it uses the "
                                   "following\n\tfeature(s) not supported on this system:\n");
             zpool_print_unsup_feat(config);
-            (void) printf("\n");
-            (void) printf("action: The pool cannot be accessed in "
+            (void) fprintf(stderr, "\n");
+            (void) fprintf(stderr, "action: The pool cannot be accessed in "
                                   "read-write mode. Import the pool with\n"
                                   "\t\"-o readonly=on\", access the pool from a system that "
                                   "supports the\n\trequired feature(s), or restore the "
@@ -175,20 +175,20 @@ zpool_print_vdev(zpool_handle_t * zhp, void * data) {
             break;
 
         case ZPOOL_STATUS_FAULTED_DEV_R:
-            (void) printf("status: One or more devices are "
+            (void) fprintf(stderr, "status: One or more devices are "
                                   "faulted in response to persistent errors.\n\tSufficient "
                                   "replicas exist for the pool to continue functioning "
                                   "in a\n\tdegraded state.\n");
-            (void) printf("action: Replace the faulted device, "
+            (void) fprintf(stderr, "action: Replace the faulted device, "
                                   "or use 'zpool clear' to mark the device\n\trepaired.\n");
             break;
 
         case ZPOOL_STATUS_FAULTED_DEV_NR:
-            (void) printf("status: One or more devices are "
+            (void) fprintf(stderr, "status: One or more devices are "
                                   "faulted in response to persistent errors.  There are "
                                   "insufficient replicas for the pool to\n\tcontinue "
                                   "functioning.\n");
-            (void) printf("action: Destroy and re-create the pool "
+            (void) fprintf(stderr, "action: Destroy and re-create the pool "
                                   "from a backup source.  Manually marking the device\n"
                                   "\trepaired using 'zpool clear' may allow some data "
                                   "to be recovered.\n");
@@ -196,29 +196,29 @@ zpool_print_vdev(zpool_handle_t * zhp, void * data) {
 
         case ZPOOL_STATUS_IO_FAILURE_WAIT:
         case ZPOOL_STATUS_IO_FAILURE_CONTINUE:
-            (void) printf("status: One or more devices are "
+            (void) fprintf(stderr, "status: One or more devices are "
                                   "faulted in response to IO failures.\n");
-            (void) printf("action: Make sure the affected devices "
+            (void) fprintf(stderr, "action: Make sure the affected devices "
                                   "are connected, then run 'zpool clear'.\n");
             break;
 
         case ZPOOL_STATUS_BAD_LOG:
-            (void) printf("status: An intent log record "
+            (void) fprintf(stderr, "status: An intent log record "
                                   "could not be read.\n"
                                   "\tWaiting for adminstrator intervention to fix the "
                                   "faulted pool.\n");
-            (void) printf("action: Either restore the affected "
+            (void) fprintf(stderr, "action: Either restore the affected "
                                   "device(s) and run 'zpool online',\n"
                                   "\tor ignore the intent log records by running "
                                   "'zpool clear'.\n");
 
         case ZPOOL_STATUS_HOSTID_MISMATCH:
-            (void) printf("status: Mismatch between pool hostid "
+            (void) fprintf(stderr, "status: Mismatch between pool hostid "
                                   "and system hostid on imported pool.\n\tThis pool was "
                                   "previously imported into a system with a different "
                                   "hostid,\n\tand then was verbatim imported into this "
                                   "system.\n");
-            (void) printf("action: Export this pool on all systems "
+            (void) fprintf(stderr, "action: Export this pool on all systems "
                                   "on which it is imported.\n"
                                   "\tThen import it to correct the mismatch.\n");
             break;
@@ -229,7 +229,7 @@ zpool_print_vdev(zpool_handle_t * zhp, void * data) {
     }
 
     if (msgId != NULL)
-        (void) printf("   see: http://zfsonlinux.org/msg/%s\n", msgId);
+        (void) fprintf(stderr, "   see: http://zfsonlinux.org/msg/%s\n", msgId);
 
     if (config != NULL) {
         nvlist_t ** spares;
@@ -278,7 +278,7 @@ print_status_config(zpool_handle_t * zhp, const char * name, nvlist_t * nv,
     if (nvlist_lookup_uint64(nv, ZPOOL_CONFIG_NOT_PRESENT, &notpresent) == 0) {
         char *path;
         verify(nvlist_lookup_string(nv, ZPOOL_CONFIG_PATH, &path) == 0);
-        (void) printf("  was %s", path);
+        (void) fprintf(stderr, "  was %s", path);
 
     } else if (vs->vs_aux != 0) {
         switch (vs->vs_aux) {
